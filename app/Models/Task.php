0664e959762b94c4faf61cd0f033e38904e17e2b
haind <haind@thefirstone.jp>
2024-02-21 15:25:05 +0700

    CRUD User + list Tasks

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Task extends Model
 {
@@ -16,4 +17,46 @@ class Task extends Model
         'start_date',
         'end_date',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function scopeUserEmail(Builder $query, $email): Builder
+    {
+        return $query->whereHas('user', function ($query) use ($email) {
+            $query->where('email', 'like', '%' . $email . '%');
+        });
+    }
+
+    public function scopeDescription(Builder $query, $description): Builder
+    {
+        return $query->where('description', 'like', '%' . $description . '%');
+    }
+
+    public function scopeTitle(Builder $query, $title): Builder
+    {
+        return $query->where('title', 'like', '%' . $title . '%');
+    }
+
+    public function scopeStartDateFrom(Builder $query, $startDate): Builder
+    {
+        return $query->where('start_date', '>=', $startDate);
+    }
+
+    public function scopeStartDateTo(Builder $query, $startDate): Builder
+    {
+        return $query->where('start_date', '<=', $startDate);
+    }
+
+    public function scopeEndDateFrom(Builder $query, $endDate): Builder
+    {
+        return $query->where('end_date', '>=', $endDate);
+    }
+
+    public function scopeEndDateTo(Builder $query, $endDate): Builder
+    {
+        return $query->where('end_date', '<=', $endDate);
+    }
 }
